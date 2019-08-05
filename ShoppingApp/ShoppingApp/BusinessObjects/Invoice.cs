63fbcb3c7b323c9eb3719600e411644cfcd8b122
sms7182 <sms7182@gmail.com>
2019-08-05 22:03:40 +0430

    add models

---
+++ ShoppingApp/ShoppingApp/BusinessObjects/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ShoppingBusinessObject
+{
+    
+   public class Invoice
+    {
+        public Invoice()
+        {
+            InvoiceItems=new List<InvoiceItem>();
+        }
+        public UserInfo User { get; set; }
+        public Store Store { get; set; }
+        public string Code { get; set; }
+        public Status Status { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal NetPrice { get; set; }
+        public decimal IncPrice { get; set; }
+        public decimal DecPrice { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public List<InvoiceItem> InvoiceItems { get; set; }
+    }
+}
---
+++ ShoppingApp/ShoppingApp/BusinessObjects/InvoiceItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBusinessObject
+{
+    public class InvoiceItem
+    {
+        public string Code { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal NetPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal IncPrice { get; set; }
+        public decimal DecPrice { get; set; }
+        public string ItemNumber { get; set; }
+    }
+}
