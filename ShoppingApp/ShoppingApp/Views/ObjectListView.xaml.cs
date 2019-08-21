77770d64c73ad5c68576880d6487a8d66e484d2d
sms7182 <sms7182@gmail.com>
2019-08-21 21:52:55 +0430

    working good with barcode scanner and updated list

--- ShoppingApp/ShoppingApp/Views/ObjectListView.xaml.cs
+++ ShoppingApp/ShoppingApp/Views/ObjectListView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,7 +51,7 @@ namespace ShoppingApp.Views
             }
             scanner.TopText = "Hold the camera up to  the barcode ";
             scanner.BottomText = "wait for the barcode automatically  scan!";
-            //ZXing.Result result = await scanner.Scan();
+           
             ZXing.Result result = await scanner.Scan(mobileBarcodeScanningOptions);
             HandleResult(result);
         }
@@ -64,39 +65,41 @@ namespace ShoppingApp.Views
                 var dataGridItemsSource = this.devgrid.ItemsSource; 
                 if (dataGridItemsSource != null)
                 {
-                   // var invoiceItems = dataGridItemsSource.Cast<InvoiceItem>().ToList();
-                   // if (invoiceItems.Any(d => d.ItemName == result.Text))
-                   // {
-                        
-                   //     for (int i = 0; i < invoiceItems.Count; i++)
-                   //     {
-                   //         if (invoiceItems[i].ItemName == result.Text)
-                   //         {
-                   //             invoiceItems[i].Quantity = invoiceItems[i].Quantity + 1;
-                   //             break;
-                                
-                   //         }
-                   //     }
-                   // }
-                   // else
-                   // {
-                   //     var invoiceItem = new InvoiceItem();
-                   //     invoiceItem.Quantity = 1;
-                   //     invoiceItem.ItemName = result.Text;
-                   //     invoiceItem.Unit = "Number";
-                   //     invoiceItem.UnitPrice = 750;
-                   //     invoiceItem.TotalPrice = 750;
+                    var invoiceItems = ((BindingList<InvoiceItem>) dataGridItemsSource);
 
-                   //     invoiceItems.Add(invoiceItem);
+                  
+                    if (invoiceItems.Any(d => d.ItemName == result.Text))
+                    {
 
-                   // }
+                        for (int i = 0; i < invoiceItems.Count; i++)
+                        {
+                            if (invoiceItems[i].ItemName == result.Text)
+                            {
+                                invoiceItems[i].Quantity = invoiceItems[i].Quantity + 1;
+                                invoiceItems[i].TotalPrice = (invoiceItems[i].Quantity * invoiceItems[i].UnitPrice);
+                                break;
 
-                   // this.dataGrid.ItemsSource = invoiceItems;
-                    
-                   ////  this.dataGrid.PullToRefreshCommand.Execute(invoiceItems);
-                   // this.dataGrid.ForceLayout();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var invoiceItem = new InvoiceItem();
+                        invoiceItem.Quantity = 1;
+                        invoiceItem.ItemName = result.Text;
+                        invoiceItem.Unit = "Number";
+                        invoiceItem.UnitPrice = 750;
+                        invoiceItem.TotalPrice = 750;
+
+                        invoiceItems.Add(invoiceItem);
+
+                    }
+
+                    this.devgrid.ItemsSource = invoiceItems;
+
+                  
                 }
-            //    this.dataGrid.BatchCommit();
+           
                 
             }
         }
