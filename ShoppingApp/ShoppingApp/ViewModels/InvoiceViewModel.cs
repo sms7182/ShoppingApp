83cbaaebbcf962c82c8c21b99764fd6e421f7fae
Shabnam Ranjgar <shabnam.ranjgar@live.com>
2019-09-14 21:37:10 +0430

    commenting default value for InvoiceLines in create page

--- ShoppingApp/ShoppingApp/ViewModels/InvoiceViewModel.cs
+++ ShoppingApp/ShoppingApp/ViewModels/InvoiceViewModel.cs
@@ -81,10 +81,10 @@ namespace ShoppingApp.ViewModels
             QrCodePaymentCommand = new QRCodePaymentCommand(this);
             CreationDate = DateTime.Now;
             invoiceItems = new ObservableCollection<InvoiceItem>()
-            {
-                new InvoiceItem{Code = "02",CreationDate=DateTime.Now.AddDays(-1),Id = Guid.NewGuid(),ItemName="دستمال آشپزخانه",ItemNumber="122",Quantity = 1 ,UnitPrice = 1500,Unit="عدد",TotalPrice=1500,NetPrice =1500},
-                new InvoiceItem{Code = "03",CreationDate=DateTime.Now.AddDays(-1),Id = Guid.NewGuid(),ItemName="مایع ظرفشویی اتک",ItemNumber="331",Quantity = 2 ,UnitPrice = 1000,Unit="عدد",TotalPrice=2000,NetPrice =2000},
-            }
+            //{
+            //    new InvoiceItem{Code = "02",CreationDate=DateTime.Now.AddDays(-1),Id = Guid.NewGuid(),ItemName="دستمال آشپزخانه",ItemNumber="122",Quantity = 1 ,UnitPrice = 1500,Unit="عدد",TotalPrice=1500,NetPrice =1500},
+            //    new InvoiceItem{Code = "03",CreationDate=DateTime.Now.AddDays(-1),Id = Guid.NewGuid(),ItemName="مایع ظرفشویی اتک",ItemNumber="331",Quantity = 2 ,UnitPrice = 1000,Unit="عدد",TotalPrice=2000,NetPrice =2000},
+            //}
             ;
 
             ItemCount = invoiceItems.Count;
